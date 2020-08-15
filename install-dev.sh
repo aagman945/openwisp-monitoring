e2fb96c6659a46034104f54f1297bb488de4a9d5
Federico Capoano <federico.capoano@gmail.com>
2020-08-14 21:47:20 -0500

    [chores] Removed openwisp-users from install-dev (will use 0.3.0)

---
+++ install-dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO: removed when openwisp-controller 0.8.0 is released
+pip install -U https://github.com/openwisp/openwisp-controller/tarball/master
+# TODO: remove when openwisp-notifications 0.1 is released
+pip install -U https://github.com/openwisp/openwisp-notifications/tarball/dev
